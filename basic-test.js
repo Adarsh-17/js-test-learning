882ee97941d627a36e359ad0e3a182ad26d65d7f
Adarsh Aniket <adarshaniket28@gmail.com>
2024-08-12 13:34:39 +0530

    test: custom test added for sum, subtract

--- basic-test.js
+++ basic-test.js
@@ -1,8 +1,18 @@
-const actual = true;
-const expected = false;
+import {sum, subtract} from './math.js';
+let actual, result;
+
+actual = 5;
+result = sum(2, 3);
 
 
 // actual!==expected is an assertion, if assertion is false then we throw error
-if(actual!==expected){
-    throw new Error(`${actual} is not ${expected}`);  
+if(actual!==result){
+    throw new Error(`${actual} is not equal to ${result}`);  
+}
+
+actual = 1;
+result = subtract(3, 2);
+
+if(actual!==result){
+    throw new Error(`${actual} is not equal to ${result}`);  
 }
