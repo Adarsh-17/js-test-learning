db53337b53e29bf49193a446f50977757e4e0093
Adarsh Aniket <adarshaniket28@gmail.com>
2024-08-12 14:03:47 +0530

    test: custom implementation for testing done

--- basic-test.js
+++ basic-test.js
@@ -1,18 +1,37 @@
 import {sum, subtract} from './math.js';
 import assert from "assert";
 
-let actual, result;
 
-actual = 5;
-result = sum(2, 3);
+test("sum adds numbers", ()=>{
+    const expected = 5;
+    const result = sum(2, 3);
+    expect(result).toBe(expected);
+})
+
+test("subtract subtracts numbers", () => {
+    const expected = 1;
+    const result = subtract(3, 2);
+    expect(result).toBe(expected);
+})
+
+function expect(result){
+    return {
+        toBe(expected){
+            if(result !== expected){
+                throw new Error(`${result} is not equal to ${expected}`)
+            }
+        }
+    }
+}
 
 
-// actual!==expected is an assertion, if assertion is false then we throw error
-assert.strictEqual(actual, result);
-
-actual = 1;
-result = subtract(3, 2);
-
-assert.strictEqual(actual, result);
-//for assert error message is not that informational
-
+function test(title, callback){
+    try{
+        callback();
+        console.log(`✓ ${title}`);
+    }
+    catch(error){
+        console.error(`✕ ${title}`)
+		console.error(error)
+    }
+}
